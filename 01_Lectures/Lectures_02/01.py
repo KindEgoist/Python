29f275039b56dad4344cd3063ad59969474138cc
KindEgoist <djondevil@gmail.com>
2022-10-18 20:32:27 +0700

    Лекция №2

--- 01_Lectures/Lectures_02/01.py
+++ 01_Lectures/Lectures_02/01.py
@@ -36,12 +36,80 @@
 # print(new_string('!')) # !!!
 # print(new_string(4)) # 12
 
-def concatanatio(*params): # *- позволяет добавить аргументы
-    res: str = "" # присваеваем к переменной res тип строки
-    for item in params:
-        res += item
-    return res
+# def concatanatio(*params): # *- позволяет добавить аргументы
+#     res: str = "" # присваеваем к переменной res тип строки
+#     for item in params:
+#         res += item
+#     return res
 
-print(concatanatio('a', 's', 'd', 'w')) # asdw
-print(concatanatio('a', '1')) #a1
-#print(concatanatio(1, 2, 3, 4))
+# print(concatanatio('a', 's', 'd', 'w')) # asdw
+# print(concatanatio('a', '1')) #a1
+# #print(concatanatio(1, 2, 3, 4))
+
+# Рекурсия
+
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+# list = []
+# for e in range(1, 10):
+#     list.append(fib(e))
+# print(list)
+
+# Кортежи - это неизменяемый "список"
+
+# a = (3, 1, 41, 4)
+# print(a) # 3, 1, 41, 4 обращаться можно как со списком
+# print(a[-2]) #41
+# a[0] = 12# ошибка, нельзя изменить
+
+# t = tuple (['red', 'green', 'blue'])
+# red, green, blue = t
+# print ('r:{} g:{} b:{}'.format(red, green, blue)) 
+
+# Словари - Неупорядочнные коллекции произвольных объектов с доступом по ключу
+
+# объявление словаря. \ позволяет писать не в одну строчку данные а так как ниже. up и т.д. выступают в виде ключей
+# dictionary = {}
+# dictionary = \
+#     {
+#         'up': '↑',
+#         'left': '←',
+#         'down': '↓',
+#         'rught': '→'
+#     }
+# print(dictionary) #{'up': '↑', 'left': '←', 'down': '↓', 'rught': '→'
+# print(dictionary['left']) # ←
+
+# for k in dictionary.keys(): #перебор по ключам
+#     print(k)
+# for k in dictionary.values(): #перебор по значениям
+#     print(k)
+
+# Множества
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy() # копируем а в переменную с с = {1, 2, 3, 5, 8}
+# u = a.union(b) # объединяет a с b u = {1, 2, 3, 5, 8, 13, 21}  
+# i = a.intersection(b) # пересечение {8, 2, 5}
+# dl = a.difference(b) #{1, 3}
+# dr = b.difference(a) #{13, 21}
+
+# q = a \
+#     .union(b) \
+#     .difference(a.intersection(b)) #{1, 21, 3, 13}
+# print(a)
+# print(b)
+# print(c)
+# print(u)
+# print(i)
+# print(dl)
+# print(dr)
+# print(q)
+
+s = frozenset(a) # не изменяемые множества
+
+# Списки 
